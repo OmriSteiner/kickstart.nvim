655bd46a4b30b305ea6a83d0114164d12a5d7fd0
Omri Steiner <omri@steiners.co.il>
2024-07-31 18:45:17 +0200

    don't wrap lines

--- init.lua
+++ init.lua
@@ -20,6 +20,9 @@ vim.opt.mouse = ''
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Don't wrap around lines
+vim.opt.wrap = false
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
