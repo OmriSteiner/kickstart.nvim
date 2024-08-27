95d8d46c0900a962b5c7dc6442bddc5a7169b371
Omri Steiner <omri@steiners.co.il>
2024-08-27 11:46:00 +0200

    add helm and terraform lsp

--- init.lua
+++ init.lua
@@ -432,6 +432,8 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
+        helm_ls = {},
+        terraformls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
