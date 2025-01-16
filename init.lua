26cbe51a3beaa86ff91d7ad7f33d6d4778412d8b
Omri Steiner <omri@steiners.co.il>
2025-01-16 09:51:45 +0100

    update ensure installed lsps

--- init.lua
+++ init.lua
@@ -632,7 +632,22 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'helm', 'gotmpl' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'dockerfile',
+        'helm',
+        'gotmpl',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
