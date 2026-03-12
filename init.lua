9a5c8928d6d68bcf0fcace57fc30cfa2b0503640
Omri Steiner <omri.s@kelasys.com>
2026-03-12 11:08:22 +0100

    move to ts_ls

--- init.lua
+++ init.lua
@@ -402,13 +402,7 @@ require('lazy').setup({
         },
         helm_ls = {},
         terraformls = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         --
 
         lua_ls = {
